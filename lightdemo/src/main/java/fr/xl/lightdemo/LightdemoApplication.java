2109c64710273773ed5b28b7fc901aa2c223b7c4
LASSUS Xavier <LASSUSX@ad.agf.fr>
2019-10-24 17:14:04 +0200

    first commit

---
+++ lightdemo/src/main/java/fr/xl/lightdemo/LightdemoApplication.java
@@ -0,0 +1,13 @@
+package fr.xl.lightdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightdemoApplication.class, args);
+	}
+
+}
---
+++ lightdemo/src/test/java/fr/xl/lightdemo/LightdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.xl.lightdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
